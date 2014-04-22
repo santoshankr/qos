6df36290dad4ceba9b432cdfa914ef2ead82ea06
mseddiki <seddiki.said@gmail.com>
2014-04-22 00:34:03 -0400

    modified shaper.sh and shaper1.sh

--- bin/shaper.sh
+++ bin/shaper.sh
@@ -1,16 +1,16 @@
-#! /bin/bash
-if [ $# -lt 2 ] 
-	then
-	echo "Usage: $0 cl_ip_0 bw_fc_0 cl_ip_1 bw_fc_1	   0 < bw_fc_i <= 1"
-	exit
-fi
+#! /bin/ash
+if [ $# -lt 2 ]
+        then
+                echo "Usage: $0 cl_ip_0 bw_fc_0 cl_ip_1 bw_fc_1    0 < bw_fc_i <= 1"
+                exit
+        fi
 
-#
+
 # This is kilobits per second
-#
-CAPACITY=4096
+
+CAPACITY=6144
 SCALE=256
-IFACE="eth0"
+IFACE="br-lan"
 
 CL_IP_1=$1
 BW_FC_1=$2
@@ -18,20 +18,15 @@ BW_FC_1=$2
 CL_IP_2=$3
 BW_FC_2=$4
 
-BW_1=$[BW_FC_1 * CAPACITY / SCALE ]
-BW_2=$[BW_FC_2 * CAPACITY / SCALE ]
+BW_1=$((BW_FC_1 * CAPACITY / SCALE ))
+BW_2=$((BW_FC_2 * CAPACITY / SCALE ))
 
-#tc qdisc list
-tc qdisc del dev $IFACE root
-
-#tc qdisc list
-tc qdisc add dev $IFACE root handle 1: htb default 6
-
-#tc qdisc list
-tc class add dev $IFACE parent 1: classid 1:1 htb rate ${CAPACITY}kbit burst 0
-
-#exit
-tc class add dev $IFACE parent 1:1 classid 1:2 htb rate ${BW_1}kbit burst 0
-tc class add dev $IFACE parent 1:1 classid 1:3 htb rate ${BW_2}kbit burst 0
-tc filter add dev $IFACE protocol ip parent 1:0 prio 0 u32 match ip src $CL_IP_1 flowid 1:2
-tc filter add dev $IFACE protocol ip parent 1:0 prio 0 u32 match ip src $CL_IP_2 flowid 1:3
+tc qdisc del dev br-lan root
+tc qdisc add dev br-lan root handle 1: htb default 6
+tc class add dev br-lan parent 1: classid 1:1 htb rate ${CAPACITY}kbit burst 0
+tc class add dev br-lan parent 1:1 classid 1:2 htb rate ${BW_1}kbit burst 0
+tc class add dev br-lan parent 1:1 classid 1:3 htb rate ${BW_2}kbit burst 0
+tc qdisc add dev br-lan parent 1:2 handle 2: sfq perturb 10
+tc qdisc add dev br-lan parent 1:3 handle 3: sfq perturb 10
+tc filter add dev br-lan protocol ip parent 1:0 prio 0 u32 match ip dst $CL_IP_1 flowid 1:2
+tc filter add dev br-lan protocol ip parent 1:0 prio 0 u32 match ip dst $CL_IP_2 flowid 1:3
--- bin/shaper1.sh
+++ bin/shaper1.sh
@@ -1,31 +1,12 @@
-#! /bin/bash
-if [ $# -lt 2 ] 
-	then
-	echo "Usage: $0 cl_ip bw_fc"
-	exit
-fi
+#! /bin/ash
 
-#
-# This is kilobits per second
-#
-CAPACITY=8192
-SCALE=256
-IFACE="eth0"
-
+CAPACITY=6144
 CL_IP_1=$1
-BW_FC_1=$2
 
-BW_1=$[BW_FC_1 * CAPACITY / SCALE ]
 
-#tc qdisc list
-echo tc qdisc del dev $IFACE root
-
-#tc qdisc list
-echo tc qdisc add dev $IFACE root handle 1: htb default 6
-
-#tc qdisc list
-echo tc class add dev $IFACE parent 1: classid 1:1 htb rate ${CAPACITY}kbit burst 0
-
-#exit
-echo tc class add dev $IFACE parent 1:1 classid 1:2 htb rate ${BW_1}kbit burst 0
-echo tc filter add dev $IFACE protocol ip parent 1:0 prio 0 u32 match ip dst $CL_IP_1 flowid 1:2
+tc qdisc del dev br-lan root
+tc qdisc add dev br-lan root handle 1: htb default 6
+tc class add dev br-lan parent 1: classid 1:1 htb rate ${CAPACITY}kbit burst 0
+tc class add dev br-lan parent 1:1 classid 1:2 htb rate ${CAPACITY}kbit burst 0
+tc qdisc add dev br-lan parent 1:2 handle 2: sfq perturb 10
+tc filter add dev br-lan protocol ip parent 1:0 prio 0 u32 match ip dst $CL_IP_1 flowid 1:2
